3a12fa07b73e500bb6f37c1e975acab160234d53
Jimmy <bruno_pier@hotmail.com>
2019-12-11 18:49:08 -0300

    Working on linking pages - Create new folder componentes & file Navegacion.js

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "631069df6f2d90c7b115";
+/******/ 	var hotCurrentHash = "4f50dbc6faafa525b281";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,13 @@
+import Link from 'next/link';
+
+
 const Index = () => (
+    <div>
+        <ul>
+            <li><Link href="/"><a>Inicio</a></Link></li>
+            <li><Link href="/Nosotros"><a>Nosotros</a></Link></li>
+        </ul>
     <p>Hola mundo</p>
+    </div>
 )
 export default Index;
