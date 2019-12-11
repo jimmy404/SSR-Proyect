74c6a60e402b277b00062166e320c71f5abc8f4f
Jimmy <bruno_pier@hotmail.com>
2019-12-11 18:53:31 -0300

    Adding new linking pages

--- componentes/Navegacion.js
+++ componentes/Navegacion.js
@@ -1,3 +1,11 @@
+import Link from 'next/link';
+
+
 const Navegacion = () => (
-    
+        <ul>
+            <li><Link href="/"><a>Inicio</a></Link></li>
+            <li><Link href="/Nosotros"><a>Nosotros</a></Link></li>
+        </ul>
 );
+
+export default Navegacion;
